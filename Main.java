bfdf4656298d27f0f2dd535f930937e6df71bf40
armassaro <arthurmassaro@alunos.utfpr.edu.br>
2025-05-24 21:29:04 -0300

    Criação da Main na pasta raiz e alteração em Model.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+// Importando as classes do MVC
+import MVC_class.*;
+
+public class Main {
+    private View view = new View();
+    private Controller controller = new Controller();
+    private Model model = new Model();
+
+    public static void main(String[] args) {
+        
+    }
+}
